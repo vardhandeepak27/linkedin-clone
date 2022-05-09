5954edfef80306c12445669ba0ad37d28d4eb00d
Vardhan <vardhandeepak1234@gmail.com>
2022-05-09 22:36:50 +0530

    Added a few components in Home Page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
+import Leftside from './Leftside';
+import Main from './Main';
+import Rightside from './Rightside';
 
 const Home = (props) => {
-    return (
-        <Container>
-            <Section>
-                <h5><a>Hiring in a hurry? -</a></h5>
-                <p>Find talented pros in record time with Upwork and keep business moving.</p>
-            </Section>
-        </Container>
-    )
+  return (
+    <Container>
+      <Section>
+        <h5><a>Hiring in a hurry? -</a></h5>
+        <p>Find talented pros in record time with Upwork and keep business moving.</p>
+      </Section>
+      <Layout>
+        <Leftside />
+        <Main />
+        <Rightside />
+
+      </Layout>
+    </Container>
+  )
 }
 
 const Container = styled.div`
@@ -39,6 +48,21 @@ const Section = styled.section`
     font-weight: 600;
   }
   @media (max-width: 768px) {
+    flex-direction: column;
+    padding: 0 5px;
+  }
+`;
+
+const Layout = styled.div`
+  display: grid;
+  grid-template-areas: "leftside main rightside";
+  grid-template-columns: minmax(0, 5fr) minmax(0, 12fr) minmax(300px, 7fr);
+  column-gap: 25px;
+  row-gap: 25px;
+  /* grid-template-row: auto; */
+  margin: 25px 0;
+  @media (max-width: 768px) {
+    display: flex;
     flex-direction: column;
     padding: 0 5px;
   }
---
+++ src/components/Rightside.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Rightside = () => {
+    return (
+        <Container>
+            Rightside
+        </Container>
+    )
+}
+
+const Container = styled.div`
+grid-area:Rightside ;
+`;
+
+export default Rightside;
